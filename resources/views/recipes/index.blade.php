51ea9e49f3c8da967ed0d2ce9ca6751d3fadf354
mcraevenesperida@gmail.com <mcraevenangelo@gmail.com>
2022-08-22 22:11:06 +0800

    Procfile for Heroku

--- resources/views/recipes/index.blade.php
+++ resources/views/recipes/index.blade.php
@@ -1,5 +1,5 @@
 <x-layout>
-    <h1 class="md:text-4xl text-2xl p-4 font-bold text-center"> Latest Recipes</h1>
+    <h1 class="md:text-4xl text-2xl p-4 font-bold "> Latest Recipes</h1>
     @unless(count($recipes) == 0)
         <div class="px-2 py-7 md:grid md:grid-cols-2 md:gap-20 lg:grid-cols-4 lg:gap-5  ">
             @foreach ($recipes as $recipe)
